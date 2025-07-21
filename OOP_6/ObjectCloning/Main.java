c67801ad103e9981c3d5d27e49c149f3b34de9fa
aaryyya <aryampatil2005@gmail.com>
2025-07-21 09:40:28 +0530

    OOP6 Updated

--- OOP_6/ObjectCloning/Main.java
+++ OOP_6/ObjectCloning/Main.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws CloneNotSupportedException{
     Human kunal=new Human(34,"Kunal");
     Human twin =(Human)kunal.clone();
     System.out.println(twin.age+" "+twin.name);
+    System.out.println(Arrays.toString(twin.arr));
+    twin.arr[0]=100;
+    System.out.println(Arrays.toString(twin.arr));
+    System.out.println(Arrays.toString(kunal.arr));
   }  
 }
